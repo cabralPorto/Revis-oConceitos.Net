ae1c8e4168dfcf2aa8b6fa053d420e4327f8520c
cabralPorto <ronaldocabralporto@gmail.com>
2022-01-19 22:18:52 -0300

    Menu de Interação com usuario unsando.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                    //TODO: adicionar aluno
+                    break;
+                    
+                    case "2":
+                    //TODO: listar alunos
+                    break;
+
+                    case "3":
+                    //TODO: calcular media geral
+                    break;
+
+                    default:
+                            throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+          
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno ");
+            Console.WriteLine("2- Listar alunos ");
+            Console.WriteLine("3- Calcular média geral ");
+            Console.WriteLine("X- Sair ");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
         }
     }
 }
